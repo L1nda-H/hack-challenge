80d6de722b30b22e09115db33acc2920cd232d9f
Linda Hu <56267986+L1nda-H@users.noreply.github.com>
2023-11-29 22:21:41 -0500

    Create post model

--- src/db.py
+++ src/db.py
@@ -7,29 +7,77 @@ tutee_table = db.Table(
     "tutee association",
     db.Model.metadata,
     db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
 )
 tutor_table = db.Table(
     "tutor association",
     db.Model.metadata,
     db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
 )
 
+
 # your classes here
+class Post(db.Model):
+    """
+    Post Model
+    """
+
+    __tablename__ = "post"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    availability = db.Column(db.String, nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey("user.id"))
+    course = db.Column(db.Integer, db.ForeignKey("course.id"))
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Post object
+        """
+        self.title = kwargs.get("title", "")
+        self.content = kwargs.get("content", "")
+        self.availability = kwargs.get("availability", "")
+        self.user = kwargs.get("user", "")
+        self.course = kwargs.get("course", "")
+
+    def serialize(self):
+        """
+        Serialize Post object
+        """
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "availability": self.availability,
+            "user": self.user.simple_serialize(),
+            "course": self.course.simple_serialize(),
+        }
+
+    def simple_serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "availability": self.availability,
+        }
 
 
 class Course(db.Model):
     """
     Course Model
     """
+
     __tablename__ = "course"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     tutors = db.relationship(
-        "User", secondary=tutor_table, back_populates="tutor_courses")
+        "User", secondary=tutor_table, back_populates="tutor_courses"
+    )
     tutees = db.relationship(
-        "User", secondary=tutee_table, back_populates="tutee_courses")
+        "User", secondary=tutee_table, back_populates="tutee_courses"
+    )
+    posts = db.relationship("Post", cascade="delete")
 
     def __init__(self, **kwargs):
         """
@@ -45,7 +93,8 @@ class Course(db.Model):
             "id": self.id,
             "name": self.name,
             "tutors": [u.simple_serialize() for u in self.tutors],
-            "tutees": [u.simple_serialize() for u in self.tutees]
+            "tutees": [u.simple_serialize() for u in self.tutees],
+            "posts": [u.simple_serialize() for u in self.posts],
         }
 
     def simple_serialize(self):
@@ -57,19 +106,24 @@ class Course(db.Model):
             "name": self.name,
         }
 
+
 class User(db.Model):
     """
     User Model
     """
+
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     netid = db.Column(db.String, nullable=False)
     type = db.Column(db.String, nullable=True)
     tutor_courses = db.relationship(
-        "Course", secondary=tutor_table, back_populates="instructors")
+        "Course", secondary=tutor_table, back_populates="instructors"
+    )
     tutee_courses = db.relationship(
-        "Course", secondary=tutee_table, back_populates="students")
+        "Course", secondary=tutee_table, back_populates="students"
+    )
+    posts = db.relationship("Post", cascade="delete")
 
     def __init__(self, **kwargs):
         """
@@ -93,15 +147,12 @@ class User(db.Model):
             "id": self.id,
             "name": self.name,
             "netid": self.netid,
-            "courses": [c.simple_serialize() for c in courses]
+            "courses": [c.simple_serialize() for c in courses],
+            "posts": [c.simple_serialize() for c in self.posts],
         }
 
     def simple_serialize(self):
-        """"
+        """ "
         Serialize User object without the courses field
         """
-        return {
-            "id": self.id,
-            "name": self.name,
-            "netid": self.netid
-        }
+        return {"id": self.id, "name": self.name, "netid": self.netid}
